69104e8c876f9f1c2a5c58d44455feff3660cbdd
Ivan Ermolaev <ermolaevym@gmail.com>
2018-03-26 01:48:40 +0300

    refactoring

--- src/main/java/com/mega/mailserver/service/MailService.java
+++ src/main/java/com/mega/mailserver/service/MailService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.mail.Email;
-import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.util.MimeMessageParser;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
@@ -17,14 +16,13 @@ import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -33,55 +31,41 @@ public class MailService {
 
     private final EmailConfig emailConfig;
 
-    public void send(final SendEmailDto message) {
+    public void send(final SendEmailDto message) throws Exception {
         Email email = emailConfig.getEmail(message.getSender(), message.getFullName());
 
-        final List<InternetAddress> recipients = message.getTo()
-                .stream()
-                .map(address -> {
-                    try {
-                        return InternetAddress.parse(address)[0];
-                    } catch (AddressException e) {
-                        log.error("Cannot parse recipients' emails", e);
-                        throw new RuntimeException(e);
-                    }
-                }).collect(Collectors.toList());
+        final String recipientString = String.join(",",message.getTo());
+        final List<InternetAddress> recipients = Arrays.asList(InternetAddress.parse(recipientString));
 
-        try {
-            email.setTo(recipients);
-        } catch (EmailException e) {
-            log.error("Cannot set recipients", e);
-            throw new RuntimeException(e);
-        }
-
-        try {
-            email.setMsg(message.getText());
-        } catch (EmailException e) {
-            log.error("Cannot set message", e);
-            throw new RuntimeException(e);
-        }
-
-        try {
-            email.send();
-        } catch (EmailException e) {
-            log.error("Cannot send email ", e);
-            throw new RuntimeException(e);
-        }
+        email.setTo(recipients);
+        email.setMsg(message.getText());
+        email.send();
+        log.info("{} sended mail to [{}]", message.getSender(), recipients);
     }
 
     @RabbitListener(queues = "haraka.emails")
-    public void receive(final Message message) {
+    public void receive(final Message message) throws Exception {
+
+        MimeMessage mimeMessage = parseMimeMessage(message);
+        ReceiveEmailDto receiveEmail = parseContent(mimeMessage);
+
+        log.info("From: {}, Message: {}", receiveEmail.getFrom(), receiveEmail.getText());
+    }
+
+    private MimeMessage parseMimeMessage(final Message message) {
         Session s = Session.getInstance(new Properties());
         InputStream is = new ByteArrayInputStream(message.getBody());
 
-        MimeMessage mail;
         try {
-            mail = new MimeMessage(s, is);
+            return new MimeMessage(s, is);
         } catch (MessagingException e) {
             log.error("Cannot parse email message: {}", message, e);
             throw new RuntimeException(e);
         }
-        MimeMessageParser parser = new MimeMessageParser(mail);
+    }
+
+    private ReceiveEmailDto parseContent(final MimeMessage mimeMessage) {
+        MimeMessageParser parser = new MimeMessageParser(mimeMessage);
 
         try {
             parser.parse();
@@ -90,23 +74,20 @@ public class MailService {
             throw new RuntimeException(e);
         }
 
-        ReceiveEmailDto receiveEmail;
         try {
-            receiveEmail = ReceiveEmailDto.builder()
+            final String content = formatContent(parser);
+            return ReceiveEmailDto.builder()
                     .from(parser.getFrom())
-                    .text(getText(parser))
+                    .text(content)
                     .build();
         } catch (Exception e) {
             log.error("Cannot get parsed email content", e);
             throw new RuntimeException(e);
         }
-
-
-        log.info("From: {}, Message: {}", receiveEmail.getFrom(), receiveEmail.getText());
     }
 
-    private String getText(final MimeMessageParser parser) {
-        if(StringUtils.isBlank(parser.getPlainContent())) {
+    private String formatContent(final MimeMessageParser parser) {
+        if (StringUtils.isBlank(parser.getPlainContent())) {
             String html = parser.getHtmlContent();
             return Jsoup.clean(html, Whitelist.basic());
         } else {
