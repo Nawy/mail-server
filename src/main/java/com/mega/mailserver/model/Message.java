23bed359902493c48df013e2cbeabee343b3e1bd
taiberium <akhmetovrustam1990@gmail.com>
2018-03-25 13:31:26 +0300

    objects created

---
+++ src/main/java/com/mega/mailserver/model/Message.java
@@ -0,0 +1,20 @@
+package com.mega.mailserver.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    @Id
+    private String id;
+    private String senderEmail;
+    private String recipientEmail;
+    private String text;
+    private LocalDateTime time;
+}
