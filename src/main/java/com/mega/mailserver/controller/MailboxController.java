da2182607d917bb4df13a8095ae49d20fca0e878
Ivan Ermolaev <ermolaevym@gmail.com>
2018-04-10 23:47:41 +0300

    added send service fully

--- src/main/java/com/mega/mailserver/controller/MailboxController.java
+++ src/main/java/com/mega/mailserver/controller/MailboxController.java
@@ -7,10 +7,7 @@ import com.mega.mailserver.service.MailboxService;
 import com.mega.mailserver.service.security.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
--- src/main/java/com/mega/mailserver/service/PostService.java
+++ src/main/java/com/mega/mailserver/service/PostService.java
@@ -1,9 +1,17 @@
 package com.mega.mailserver.service;
 
+import com.mega.mailserver.config.EmailProperties;
+import com.mega.mailserver.model.EmailAddress;
 import com.mega.mailserver.model.domain.Letter;
 import com.mega.mailserver.model.domain.User;
+import com.mega.mailserver.model.enums.LetterDirection;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Address;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
@@ -11,15 +19,36 @@ public class PostService {
 
     private final SmtpService smtpService;
     private final MailboxService mailboxService;
+    private final EmailProperties emailProperties;
 
-    public void send(final Letter letter, final User user) {
+    public void send(final Letter letter, final User user) throws AddressException {
 
-        try {
-            smtpService.send(letter, user);
-        } catch(Exception e) {
-            throw new RuntimeException("Cannot send message");
+        final EmailAddress fromAddress = new EmailAddress(user.getName(), emailProperties.getDomain());
+        final EmailAddress toAddress = new EmailAddress(letter.getAddress());
+
+        final String domain = toAddress.getDomain();
+        if (!isLocalDomain(domain)) {
+            try {
+                smtpService.send(letter, user);
+            } catch (Exception e) {
+                throw new RuntimeException("Cannot send message");
+            }
         }
 
-        mailboxService.put(letter, user.getName());
+        final Letter inboxEmail = letter.toBuilder()
+                .address(fromAddress.getAddress().getAddress())
+                .direction(LetterDirection.INBOX)
+                .build();
+
+        final Letter outboxEmail = letter.toBuilder()
+                .direction(LetterDirection.OUTBOX)
+                .build();
+
+        mailboxService.put(inboxEmail, toAddress.getName());
+        mailboxService.put(outboxEmail, user.getName());
+    }
+
+    private boolean isLocalDomain(final String domain) {
+        return domain.equalsIgnoreCase(emailProperties.getDomain());
     }
 }
